b86c3da7744019e35be3ebf6f0692b594783df86
ujjawal8461 <101388857+ujjawal8461@users.noreply.github.com>
2023-10-07 23:23:53 +0530

    Add files via upload

---
+++ asthetic Landing Page/script.js
@@ -0,0 +1,43 @@
+TweenMax.from('.div1', 1,{
+    delay:.2,
+    left:'100%',
+    ease:Expo.easeIn
+})
+
+TweenMax.from('.div2', 1,{
+    delay:.4,
+    left:'100%',
+    ease:Expo.easeIn
+})
+
+TweenMax.from('.div3', 1,{
+    delay:.6,
+    left:'100%',
+    ease:Expo.easeIn
+})
+
+TweenMax.from('.div4', 1,{
+    delay:.8,
+    left:'100%',
+    ease:Expo.easeIn
+})
+
+TweenMax.from('.title1', 2,{
+    delay:1.8,
+    opacity:0,
+    x:20,
+    ease:Circ.easeInOut
+})
+TweenMax.from('.title2', 2,{
+    delay:1.8,
+    opacity:0,
+    x:-20,
+    ease:Circ.easeInOut
+})
+
+TweenMax.from('.filter-vintage', 2,{
+    delay:2,
+    opacity:0,
+    y:20,
+    ease:Expo.easeOut
+})
